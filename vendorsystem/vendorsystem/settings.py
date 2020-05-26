c74357cf94c83d49ff2e7c649f3e16eb6d94f595
jemikalajah123 <philip.jemikalajah@konga.local>
2020-05-26 16:46:31 +0100

    update file

--- vendorsystem/vendorsystem/settings.py
+++ vendorsystem/vendorsystem/settings.py
@@ -56,6 +56,7 @@ AUTH_USER_MODEL = 'food.Auth'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
