80806b648c2e95c6bb9a11476253fe6add96f911
jemikalajah123 <philip.jemikalajah@konga.local>
2020-05-26 16:37:19 +0100

    update file

--- vendorsystem/vendorsystem/settings.py
+++ vendorsystem/vendorsystem/settings.py
@@ -138,7 +138,12 @@ EMAIL_PORT = 465
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
