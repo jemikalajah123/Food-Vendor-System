faff28e68c22180681334e4b1d83a50a2f2167b3
jemikalajah123 <philip.jemikalajah@konga.local>
2020-05-26 07:42:17 +0100

    modified settings file

--- vendorsystem/vendorsystem/settings.py
+++ vendorsystem/vendorsystem/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'oj+*8=o6%aj3e2s11_texd*uu&ql&^%0#-3w(rs&fy1n3hg45@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -140,3 +140,4 @@ EMAIL_PORT = 465
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
